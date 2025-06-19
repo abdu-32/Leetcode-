24d179870769ca729cfc50ad1336dafea4580dff
abdu-32 <157193024+abdu-32@users.noreply.github.com>
2025-06-19 12:31:21 +0300

    Time: 7 ms (38.18%) | Memory: 19.7 MB (6.76%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sets = set()
+        lenght = len(nums)
+        for i in range(lenght + 1):
+            sets.add(i)
+        diff = sets.difference(nums)
+        for res in diff:
+            return res
+    
+
+        
